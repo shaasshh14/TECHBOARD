06dfbeda057d50c70fe95b75f0ba14c6b97404b0
Mayank-jjn <mayankdk605@gmail.com>
2025-09-01 23:22:51 +0530

    updation_of_team

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header.jsx";
-
+import Spline from "@splinetool/react-spline";
 function Team() {
   return (
     <div>
       <Header />
+      <Spline
+        className="absolute x1:right-[-28%] right-0 top-[-20%] lg:top-0"
+        scene="https://prod.spline.design/AykasHNRalnfTDfs/scene.splinecode"
+      />
 
       <div style={{ padding: "2rem" }}>
         <h1>Team Page</h1>
