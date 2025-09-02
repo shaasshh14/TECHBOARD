8d9f2c98627b850f0da095c6d64d304d0c395f8a
Mohit-Kumar-Dhaka <dhakaji725@gmail.com>
2025-09-02 22:08:24 +0530

    making cards on team page

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -2,18 +2,29 @@ import Header from "../components/Header.jsx";
 import Spline from "@splinetool/react-spline";
 import ParticlesComponent from "../components/Particles.jsx";
 import Footer from "../components/FooterCTA.jsx";
+import ProfileCard from "../components/ProfileCard.jsx";
 function Team() {
   return (
     <div>
       <ParticlesComponent id="particles"/>
       <Header />
 
-      <section className="h-screen  xl:flex-row flex-col-reverse items-center justify-between lg:px-24 px-10 relative overflow-hidden">
-        <Spline
-          className="absolute xl:right-[-30%] xl:top-[0%] right-0 top-0 w-[600px] h-[600px]"
-          scene="https://prod.spline.design/AykasHNRalnfTDfs/scene.splinecode"
-        />
-      </section>
+      {/* <section className="h-screen  xl:flex-row flex-col-reverse items-center justify-between lg:px-24 px-10 relative overflow-hidden">
+
+
+      </section> */}
+       <ProfileCard
+  name="Javi A. Torres"
+  title="Software Engineer"
+  handle="javicodes"
+  status="Online"
+  contactText="Contact Me"
+  avatarUrl="/path/to/avatar.jpg"
+  showUserInfo={true}
+  enableTilt={true}
+  enableMobileTilt={false}
+  onContactClick={() => console.log('Contact clicked')}
+/>
       <Footer/>
     </div>
   );
